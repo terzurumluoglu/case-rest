6ad56128c058b9c958b9253dd8ce957b2adbc37d
Toprak Erzurumluoğlu <toprakerzurumluoglu@hotmail.com>
2023-04-17 23:54:39 +0300

    error handler added

---
+++ src/api/middleware/errorHandler.js
@@ -0,0 +1,14 @@
+const errorHandler = (err, req, res, next) => {
+    console.error('error handler');
+
+    let error = { ...err };
+    error.message = err.message;
+
+    res.status(error.statusCode || 500).json({
+        success: false,
+        statusCode: error.statusCode,
+        error: error.message || 'Server Error',
+    });
+}
+
+module.exports = errorHandler;
