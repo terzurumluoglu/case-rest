8c084c561632bd125ebe0cdf693f3812357a098d
Toprak Erzurumluoğlu <toprakerzurumluoglu@hotmail.com>
2023-04-17 23:50:47 +0300

    changed index (main) file

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
-const { server } = require('./src/server');
+const app = require('./src/app/server');
+const PORT = process.env.PORT || 8080;
 
-server.get('/', (req, res, next) => {
-    const data = {
-        message: 'Hello World!',
-    }
-    res.status(200).send(data);
+app.listen(PORT, () => {
+  const message = process.env.MESSAGE.split('{{PORT}}').join(PORT)
+  console.log(message);
+}).on('error', (error) => {
+  console.log(error.code === 'EADDRINUSE' ? ERROR_MESSAGES[error.code] : error.code);
 });
