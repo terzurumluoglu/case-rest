7887190b752d441f422bf8026bf029e40e9ac201
Toprak Erzurumluoğlu <toprakerzurumluoglu@hotmail.com>
2023-04-18 11:01:13 +0300

    add hello world root path

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 const app = require('./src/app/server');
 const PORT = process.env.PORT || 8080;
 
+app.get('/', (req, res, next) => {
+  const data = {
+      message: 'Hello World!',
+  }
+  res.status(200).send(data);
+});
+
 app.listen(PORT, () => {
   const message = process.env.MESSAGE.split('{{PORT}}').join(PORT)
   console.log(message);
